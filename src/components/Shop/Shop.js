5897606df1d9c8af3d8c2a66536c171a1874a631
SharminAkter3 <mssharmin4145@gmail.com>
2022-11-05 00:39:42 -0700

    last commit

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,60 +1,99 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { addToDb, deleteShoppingCart, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
-const Shop = () => {
-    const products = useLoaderData();
-    const [cart, setCart] = useState([]);
+/**
+ count
+ perPage (size) : 10
+ pages: count/perPage
+ CurrentPage (page)
+ */
 
-    const clearCart = () =>{
+const Shop = () => {
+    // const { products, count } = useLoaderData();
+    const [products, setProducts] = useState([]);
+    const [count, setCount] = useState(0);
+    const [cart, setCart] = useState([]);
+    const [page, setPage] = useState(0);
+    const [size, setSize] = useState(10);
+
+    useEffect(() => {
+        const url = `http://localhost:5000/products?page=${page}&size=${size}`;
+        console.log(page, size)
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setCount(data.count);
+                setProducts(data.products);
+            })
+    }, [page, size])
+
+    const pages = Math.ceil(count / size);
+
+    // const perPage = 10;
+
+    const clearCart = () => {
         setCart([]);
         deleteShoppingCart();
     }
 
-    useEffect( () =>{
+    useEffect(() => {
         const storedCart = getStoredCart();
         const savedCart = [];
-        for(const id in storedCart){
-            const addedProduct = products.find(product => product.id === id);
-            if(addedProduct){
-                const quantity = storedCart[id];
-                addedProduct.quantity = quantity;
-                savedCart.push(addedProduct);
-            }
-        }
-        setCart(savedCart);
+        const ids = Object.keys(storedCart);
+        console.log(ids);
+        fetch('http://localhost:5000/productsByIds', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(ids)
+        })
+            .then(res => res.json())
+            .then(data => {
+                for (const id in storedCart) {
+                    const addedProduct = data.find(product => product._id === id);
+                    if (addedProduct) {
+                        const quantity = storedCart[id];
+                        addedProduct.quantity = quantity;
+                        savedCart.push(addedProduct);
+                    }
+                }
+                setCart(savedCart);
+            })
+
     }, [products])
 
-    const handleAddToCart = (selectedProduct) =>{
+    const handleAddToCart = (selectedProduct) => {
         console.log(selectedProduct);
         let newCart = [];
-        const exists = cart.find(product => product.id === selectedProduct.id);
-        if(!exists){
+        const exists = cart.find(product => product._id === selectedProduct._id);
+        if (!exists) {
             selectedProduct.quantity = 1;
             newCart = [...cart, selectedProduct];
         }
-        else{
-            const rest = cart.filter(product => product.id !== selectedProduct.id);
+        else {
+            const rest = cart.filter(product => product._id !== selectedProduct._id);
             exists.quantity = exists.quantity + 1;
             newCart = [...rest, exists];
         }
-        
+
         setCart(newCart);
-        addToDb(selectedProduct.id);
+        addToDb(selectedProduct._id);
     }
 
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product=><Product 
+                    products.map(product => <Product
                         key={product.id}
                         product={product}
                         handleAddToCart={handleAddToCart}
-                        ></Product>)
+                    ></Product>)
                 }
             </div>
             <div className="cart-container">
@@ -63,6 +102,24 @@ const Shop = () => {
                         <button>Review Order</button>
                     </Link>
                 </Cart>
+            </div>
+            <div className="pagination">
+                <h2>Currently selected page : {page} and size: {size}</h2>
+                {
+                    [...Array(pages).keys()].map(number => <button
+                        key={number}
+                        className={page === number && 'selected'}
+                        onClick={() => setPage(number)}
+                    >
+                        {number + 1}
+                    </button>)
+                }
+                <select onChange={event => setSize(event.target.value)}>
+                    <option value="5">5</option>
+                    <option value="10" selected>10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                </select>
             </div>
         </div>
     );
--- src/loaders/productsAndCartLoader.js
+++ src/loaders/productsAndCartLoader.js
@@ -2,15 +2,15 @@ import { getStoredCart } from "../utilities/fakedb";
 
 export const productsAndCartLoader = async () => {
     // get Products
-    const productsData = await fetch('products.json');
-    const products = await productsData.json();
+    const productsData = await fetch('http://localhost:5000/products');
+    const { products } = await productsData.json();
 
     // get cart
     const savedCart = getStoredCart();
     const initialCart = [];
-    
+
     for (const id in savedCart) {
-        const addedProduct = products.find(product => product.id === id);
+        const addedProduct = products.find(product => product._id === id);
         if (addedProduct) {
             const quantity = savedCart[id];
             addedProduct.quantity = quantity;
